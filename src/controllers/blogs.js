f136350a96f42ff14d54e4e4ba90b7e1982726ab
Riku Rauhala <riku@rauhala.io>
2023-03-10 17:54:24 +0200

    Implement support for changing the number of likes

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -2,6 +2,11 @@ const router = require('express').Router()
 
 const { Blog } = require('../models')
 
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll()
   res.json(blogs)
@@ -16,26 +21,29 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.get('/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id)
-  if (blog) {
-    res.json(blog)
+router.get('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    res.json(req.blog)
   } else {
     res.status(404).end()
   }
 })
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const blog = await Blog.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-    res.json(blog)
-  } catch(error) {
-    return res.status(400).json({ error })
+router.put('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
   }
 })
 
-module.exports = router;
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy()
+  }
+  res.status(204).end()
+})
+
+module.exports = router
