952e7bc540b141bb880beec90ac6a7d473603913
Riku Rauhala <riku@rauhala.io>
2023-03-10 10:44:02 +0200

    Create functionality for fetching and printing database contents

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+require('dotenv').config()
+const { Sequelize, QueryTypes } = require('sequelize')
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+const main = async () => {
+  try {
+    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+    console.log("Blogs in the database:")
+    blogs.forEach(({ author, title, likes }) => {
+      console.log(`- ${author}: ${title} (${likes} likes)`)
+    })
+    sequelize.close()
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+
+main()
