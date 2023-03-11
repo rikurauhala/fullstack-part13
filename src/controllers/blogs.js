e8f3fcea9232de55fb6d087bc96ff45b7bef1d4f
Riku Rauhala <riku@rauhala.io>
2023-03-11 18:25:05 +0200

    Link the logged-in user to a new blog added

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -1,6 +1,21 @@
 const router = require('express').Router()
+const jwt = require('jsonwebtoken')
+const { Blog, User } = require('../models')
+const { SECRET } = require('../utils/config')
 
-const { Blog } = require('../models')
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch{
+      return res.status(401).json({ error: 'Invalid token' })
+    }
+  } else {
+    return res.status(401).json({ error: 'Missing token' })
+  }
+  next()
+}
 
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id)
@@ -12,8 +27,9 @@ router.get('/', async (req, res) => {
   res.json(blogs)
 })
 
-router.post('/', async (req, res) => {
-  const blog = await Blog.create(req.body)
+router.post('/', tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id)
+  const blog = await Blog.create({...req.body, userId: user.id, date: new Date()})
   return res.json(blog)
 })
 
--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -2,7 +2,7 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.name)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'Malformatted id' })
+    return response.status(400).json({ error: 'Malformatted id' })
   } 
   if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
