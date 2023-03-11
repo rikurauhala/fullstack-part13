8333b081741070f32832c18112d4f64783e52687
Riku Rauhala <riku@rauhala.io>
2023-03-11 12:13:45 +0200

    Add support for users

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ const { PORT } = require('./utils/config')
 const { connectToDatabase } = require('./utils/db')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 const middleware = require('./utils/middleware')
 
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const start = async () => {
   await connectToDatabase()
