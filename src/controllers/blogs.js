aa52c6d91b5dea643cfecc6a18b0953054c3e3ec
Riku Rauhala <riku@rauhala.io>
2023-03-12 11:18:16 +0200

    Implement filtering by keyword

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const { Op } = require('sequelize')
 const jwt = require('jsonwebtoken')
 const { Blog, User } = require('../models')
 const { SECRET } = require('../utils/config')
@@ -23,12 +24,21 @@ const blogFinder = async (req, res, next) => {
 }
 
 router.get('/', async (req, res) => {
+  const where = {}
+
+  if (req.query.search) {
+    where.title = {
+      [Op.iLike]: `%${req.query.search}%`
+    }
+  }
+
   const blogs = await Blog.findAll({
     attributes: { exclude: ['userId'] },
-      include: {
-        model: User,
-        attributes: ['name']
-    }
+    include: {
+      model: User,
+      attributes: ['name']
+    },
+    where,
   })
   res.json(blogs)
 })
