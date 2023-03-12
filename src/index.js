e60c9b9a78995ab9819beabb5579b1d63de800f7
Riku Rauhala <riku@rauhala.io>
2023-03-12 18:33:50 +0200

    Create controller for authors

--- src/index.js
+++ src/index.js
@@ -5,17 +5,19 @@ const app = express()
 const { PORT } = require('./utils/config')
 const { connectToDatabase } = require('./utils/db')
 
+const authorsRouter = require('./controllers/authors')
 const blogsRouter = require('./controllers/blogs')
-const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const usersRouter = require('./controllers/users')
 
 const middleware = require('./utils/middleware')
 
 app.use(express.json())
 
+app.use('/api/authors', authorsRouter)
 app.use('/api/blogs', blogsRouter)
-app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/users', usersRouter)
 
 const start = async () => {
   await connectToDatabase()
