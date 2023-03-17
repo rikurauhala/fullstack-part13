495177e0c87add60504b71f66bd6054da30c7338
Riku Rauhala <riku@rauhala.io>
2023-03-17 20:59:20 +0200

    Move the token extractor into utils

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -1,22 +1,7 @@
 const router = require('express').Router()
 const { Op } = require('sequelize')
-const jwt = require('jsonwebtoken')
 const { Blog, User } = require('../models')
-const { SECRET } = require('../utils/config')
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    try {
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
-    } catch{
-      return res.status(401).json({ error: 'Invalid token' })
-    }
-  } else {
-    return res.status(401).json({ error: 'Missing token' })
-  }
-  next()
-}
+const tokenExtractor = require('../utils/tokens')
 
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id)
---
+++ src/utils/tokens.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+const { SECRET } = require('../utils/config')
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch{
+      return res.status(401).json({ error: 'Invalid token' })
+    }
+  } else {
+    return res.status(401).json({ error: 'Missing token' })
+  }
+  next()
+}
+
+module.exports = tokenExtractor
